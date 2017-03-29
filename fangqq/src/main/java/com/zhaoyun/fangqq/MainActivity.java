eacaf36215f8195785a6c606f51d17a1c8228d2f
zhaohyun2013 <zhaoyun2013@hotmail.com>
2017-03-29 09:01:02 +0800

    添加一个SwipeRefreshLayout控件进行下拉刷新

--- fangqq/src/main/java/com/zhaoyun/fangqq/MainActivity.java
+++ fangqq/src/main/java/com/zhaoyun/fangqq/MainActivity.java
@@ -2,7 +2,11 @@ package com.zhaoyun.fangqq;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -19,7 +23,13 @@ public class MainActivity extends Activity {
     private DragLayout mDragLayout;
     private String TAG = MainActivity.class.getSimpleName();
     private ListView mMainList;
-
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +85,42 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this,DrawerActivity.class));
             }
         });
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipRefresh);
+        //改变加载显示的颜色
+        swipeRefreshLayout.setColorSchemeColors(Color.RED, Color.RED);
+        //设置初始时的大小
+        swipeRefreshLayout.setSize(SwipeRefreshLayout.LARGE);
+        //设置向下拉多少出现刷新
+        swipeRefreshLayout.setDistanceToTriggerSync(100);
+        //设置刷新出现的位置
+        swipeRefreshLayout.setProgressViewEndTarget(false, 200);
+        //设置监听
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            //然刷新控件停留两秒后消失
+                            Thread.sleep(2000);
+
+                            handler.post(new Runnable() {//在主线程执行
+                                @Override
+                                public void run() {
+                                    //停止刷新
+                                    swipeRefreshLayout.setRefreshing(false);
+                                }
+                            });
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+
     }
 
 
